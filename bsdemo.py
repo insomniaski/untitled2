dff9e72360a87d86869fc0813325bca7278ad32e
leo <243494752@qq.com>
2016-07-01 00:20:35 +0800

    wangyq

---
+++ bsdemo.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+import  requests
+from bs4 import BeautifulSoup
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+html = requests.get("https://movie.douban.com/top250?start=").content
+
+soup = BeautifulSoup(html,"lxml")
+
+for link in soup.find_all("span","title"):
+
+        print link.string
+
