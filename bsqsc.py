f5c42fb1ca939f5bfdfaf6fea2d81c50df56bc87
insomniaski <243494752@qq.com>
2016-07-01 16:51:34 +0800

    wangyq

--- bsqsc.py
+++ bsqsc.py
@@ -7,29 +7,31 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 i=1
 
-k=0
-while (k<10):
-        url = "https://movie.douban.com/top250?start="
+k=1
+while (k<35):
+        url = "http://www.qschou.com/index/project/projects/"
         # print k
-        url+=str(25*k)
+        url+=str(k)
         print url
         html = requests.get(url).content
 
         soup = BeautifulSoup(html,"lxml")
         tag=soup.span
         #print soup.find_all('span')
-        link=soup.find(("h1"))
-        print link
-        for i in  range(25):
+        link=soup.find(id="tab-1")
+        #print link
+        for i in  range(6):
 
-                link=link.find_next("em")
-                print link.string
+                link=link.find_next("a")
+                print link.get('title')
+                print "http://www.qschou.com"+link.get('href')
 
-                link=link.find_next("span")
-                print link.string
 
-                link = link.find_previous("a")
-                print link.get('href')
+                # link=link.find_next("span")
+                # print link.string
+                #
+                # link = link.find_previous("a")
+                # print link.get('href')
 
         # for link in soup.find_all("span",["title"]):
         #         print i
