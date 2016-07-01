36a71778a873a32ddcc02b0de86278c0739372d6
insomniaski <243494752@qq.com>
2016-07-01 18:27:21 +0800

    wangyq

--- bsqsc.py
+++ bsqsc.py
@@ -8,7 +8,7 @@ sys.setdefaultencoding('utf-8')
 i=1
 
 k=1
-while (k<35):
+while (k<20):
         url = "http://www.qschou.com/index/project/projects/"
         # print k
         url+=str(k)
@@ -21,12 +21,18 @@ while (k<35):
         link=soup.find(id="tab-1")
         #print link
         for i in  range(6):
-
+            if link is not None:
                 link=link.find_next("a")
                 print link.get('title')
-                print "http://www.qschou.com"+link.get('href')
-
-
+                url2= "http://www.qschou.com"+link.get('href')
+                print url2
+                html2=requests.get(url2).content
+                soup2=BeautifulSoup(html2,"lxml")
+                link2=soup2.find('div',"qsc-project-status_detail clearfix")
+                #print link2
+                link3=link2.find_all("strong")
+                for elem in link3: print elem.get_text()
+#
                 # link=link.find_next("span")
                 # print link.string
                 #
