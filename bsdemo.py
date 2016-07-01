b7de7444f1ab5d384e57227b5ed334e3d0f67e1c
insomniaski <243494752@qq.com>
2016-07-01 15:43:04 +0800

    wangyq

--- bsdemo.py
+++ bsdemo.py
@@ -5,12 +5,35 @@ from bs4 import BeautifulSoup
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
+i=1
 
-html = requests.get("https://movie.douban.com/top250?start=").content
+k=0
+while (k<10):
+        url = "https://movie.douban.com/top250?start="
+        # print k
+        url+=str(25*k)
+        print url
+        html = requests.get(url).content
 
-soup = BeautifulSoup(html,"lxml")
+        soup = BeautifulSoup(html,"lxml")
+        tag=soup.span
+        #print soup.find_all('span')
+        link=soup.find(("h1"))
+        print link
+        for i in  range(25):
 
-for link in soup.find_all("span","title"):
+                link=link.find_next("em")
+                print link.string
 
-        print link.string#yes
+                link=link.find_next("span")
+                print link.string
+
+                link = link.find_previous("a")
+                print link.get('href')
+
+        # for link in soup.find_all("span",["title"]):
+        #         print i
+        #         print link.string
+        #         i+=1
+        k+=1
 
